more plotting/debugging, fix derivative, less violation

mlt/raceline_mlt.py
@@ -137,7 +137,7 @@ class MLTCollocation(PSCollocation):
             # "ipopt.print_frequency_iter": 50,
             "print_time": 0,
             "ipopt.sb": "no",
-            "ipopt.max_iter": 0,
+            "ipopt.max_iter": 1000,
             "detect_simple_bounds": True,
             "ipopt.linear_solver": "ma97",
             "ipopt.mu_strategy": "adaptive",
@@ -176,16 +176,16 @@ class MLTCollocation(PSCollocation):
         v_sol = [sol.value(seg) for seg in Q_1_dot]
 
         ttt = Trajectory(Q_sol, U_sol, v_sol, t, self.track.length)
-        fine_plot, _ = self.track.plot_uniform(1)
+        fine_plot, _ = self.track.plot_uniform( 1)
 
-        x = ttt.plot_colloc()
-        xx = ttt.plot_uniform()
+        # x = ttt.plot_colloc()
+        xx = ttt.plot_uniform(np.array(all_t) * self.track.length)
 
         fig = go.Figure()
         for i in fine_plot:
             fig.add_trace(i)
 
-        fig.add_traces([x, self.track.plot_raceline_uniform(ttt), self.track.plot_raceline_colloc(all_t, ttt)])
+        fig.add_traces([xx, self.track.plot_raceline_uniform(ttt), self.track.plot_raceline_colloc(all_t, ttt)])
         fig.update_layout(scene=dict(aspectmode="data"))
         fig.show()
     @staticmethod

mlt/trajectory.py
@@ -20,12 +20,13 @@ class Trajectory:
         Args:
 
             Q (list[np.ndarray]): List of matricies representing q in each interval
-            X (list[np.ndarray]): List of matricies representing x in each interval
+            U (list[np.ndarray]): List of matricies representing u in each interval
+            v (list): List containing velocities at each mesh and collocation point
             t (np.ndarray): List of arc length parameters representing the beginning
                             of each interval
         """
-        self.Q = Q  # q
-        self.v = v
+        self.Q = Q  # q2,...
+        self.v = np.array(v) * track_length
         self.U = U  # fxa fxb delta
         self.t = t * track_length
         self.length = track_length
@@ -42,7 +43,7 @@ class Trajectory:
             tau = np.asarray([-1] + list(tau) + [1])
 
             self.poly.append(
-                scipy.interpolate.BarycentricInterpolator(tau, np.column_stack([U[k], Q[k], v[k]]))
+                scipy.interpolate.BarycentricInterpolator(tau, np.column_stack([U[k], Q[k], self.v[k]]))
             )
 
     def __call__(self, s: np.ndarray) -> np.ndarray:
@@ -96,19 +97,7 @@ class Trajectory:
             line=dict(color=self.v, colorscale="plasma"),
         )
 
-    def plot_uniform(self, approx_spacing: float = 0.1):
-        """
-        Makes Plotly GraphObjects for centerline, left/right boundaries of track, and
-        theta, mu, and phi at uniformly sampled points along the track
-
-        Args:
-            approx_spacing (float): Distance between sampled points
-
-        Returns:
-            tuple: Tuple of list of GraphObjects for centerline + left/right boundaries
-                   and GraphObject for theta/mu/phi
-        """
-
+    def plot_uniform(self, t, approx_spacing: float = 0.1):
         # Sample uniformly according to the given spacing
         s = np.linspace(0, self.length, int(self.length // approx_spacing))
         points = self(s)
@@ -116,21 +105,25 @@ class Trajectory:
         f1 = go.Figure()
         f2 = go.Figure()
         f3 = go.Figure()
+        f4 = go.Figure()
 
-        p1, p2, p3 = self.plot_colloc()
+        p1, p2, p3, p4 = self.plot_colloc(t)
 
         f1.add_trace(p1)
         f2.add_trace(p2)
         f3.add_trace(p3)
+        f4.add_trace(p4)
 
 
-        f1.add_trace(px.scatter(x=s, y=points[:, 0]))
-        f2.add_trace(px.scatter(x=s, y=points[:, 1]))
-        f3.add_trace(px.scatter(x=s, y=points[:, 2]))
+        f1.add_trace(go.Scatter(x=s, y=points[:, 0], name="uniform fxa"))
+        f2.add_trace(go.Scatter(x=s, y=points[:, 1], name="uniform fxb"))
+        f3.add_trace(go.Scatter(x=s, y=points[:, 2], name="uniform delta"))
+        f4.add_trace(go.Scatter(x=s, y=points[:, -1], name="uniform vel"))
 
         f1.show()
         f2.show()
         f3.show()
+        f4.show()
 
         return go.Scatter3d(
             x=points[:, 0],
@@ -141,28 +134,16 @@ class Trajectory:
             line=dict(color=self.v, colorscale="plasma"),
         )
 
-    def plot_colloc(self):
-        """
-        Makes Plotly GraphObjects for centerline, left/right boundaries of track, and
-        theta, mu, and phi at uniformly sampled points along the track
-
-        Args:
-            approx_spacing (float): Distance between sampled points
-
-        Returns:
-            tuple: Tuple of list of GraphObjects for centerline + left/right boundaries
-                and GraphObject for theta/mu/phi
-        """
-        s = self.t
-
+    def plot_colloc(self, s):
         # Sample uniformly according to the given spacing
         points = self(s)
 
 
         return (
-            px.scatter(x=s, y=points[:, 0], title="fxa"),
-            px.scatter(x=s, y=points[:, 1], title="fxb"),
-            px.scatter(x=s, y=points[:, 2], title="delta"),
+            go.Scatter(x=s, y=points[:, 0], name="fxa",mode='markers',),
+            go.Scatter(x=s, y=points[:, 1], name="fxb",mode='markers',),
+            go.Scatter(x=s, y=points[:, 2], name="delta",mode='markers',),
+            go.Scatter(x=s, y=points[:, -1], name="vel",mode='markers',),
         )
         # return go.Scatter3d(
         #     x=points[:, 0],

track_import/track.py
@@ -66,7 +66,7 @@ class Track:
                         for each given arc length parameter
         """
         s = s % self.length
-        k = np.searchsorted(self.t[1:], s)
+        # k = np.searchsorted(self.t[1:], s)
 
         tau, k = self.t_to_tau(s)
         return np.asarray([self.poly[interval](parameter) for parameter, interval in zip(tau, k)])
@@ -136,12 +136,12 @@ class Track:
                         [x, y, z, theta, mu, phi, n_l, n_r] for each given arc length parameter
         """
         s = s % self.length
-        k = np.searchsorted(self.t[1:], s)
+        # k = np.searchsorted(self.t[1:], s)
 
         tau, k = self.t_to_tau(s)
         return np.asarray(
             [
-                self.poly[interval].derivative(parameter, der=n)
+                self.poly[interval].derivative(parameter, der=n) * (2.0 / (self.t[interval+1] - self.t[interval])) ** n
                 for parameter, interval in zip(tau, k)
             ]
         )
@@ -293,11 +293,12 @@ class Track:
             mode="lines",
             line=dict(color=ss[:, -1] * self.length, colorscale="plasma", showscale=True),
         )
-    
+
     def plot_raceline_colloc(self, all_t, trajectory: Trajectory):
-        print(trajectory.v)
         all_t = np.array(all_t)
-        r = self.raceline(self.state(self.length * all_t), trajectory.state(all_t * self.length)[:, 3])
+        r = self.raceline(
+            self.state(self.length * all_t), trajectory.state(all_t * self.length)[:, 3]
+        )
 
         return go.Scatter3d(
             x=r[:, 0],
@@ -307,7 +308,6 @@ class Track:
             mode="markers",
             marker=dict(color=trajectory.v, colorscale="plasma"),
         )
-
 
     def raceline(self, state: np.ndarray, lateral_displacement: float) -> np.ndarray:
         """