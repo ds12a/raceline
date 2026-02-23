scaling, other possible fixes

mlt/raceline_mlt.py
@@ -55,6 +55,7 @@ class MLTCollocation(PSCollocation):
 
                 U.append(self.opti.variable(N[k] + 2, self.n_u))
                 Z.append(self.opti.variable(N[k] + 2, self.n_z))
+
             else:
                 # Explicitly couples last of previous segment with first of current segment
                 # by setting them as the same variable
@@ -115,24 +116,23 @@ class MLTCollocation(PSCollocation):
             v_guess = 10
             self.opti.set_initial(Q_1_dot[k][:, :], v_guess / self.track.length)
 
-
             # Vertical tire forces
-            # print(Z[k])
             for i in range(2):
-                downforce = self.vehicle.env.rho / 2 * self.vehicle.prop.g_S * v_guess ** 2 * self.vehicle.prop.a_Cz[i]
+                downforce = (
+                    self.vehicle.env.rho
+                    / 2
+                    * self.vehicle.prop.g_S
+                    * v_guess**2
+                    * self.vehicle.prop.a_Cz[i]
+                )
                 for j in range(2):
                     self.opti.set_initial(
-                        Z[k][:, 2 * i + j], ((self.vehicle.prop.m_sprung + self.vehicle.prop.m_unsprung) * 9.81 / 4 + downforce / 2)
+                        Z[k][:, 2 * i + j],
+                        (
+                            (self.vehicle.prop.m_sprung + self.vehicle.prop.m_unsprung) * 9.81 / 4
+                            + downforce / 2
+                        ),
                     )
-            #         print("foo")
-            #         print((self.vehicle.prop.m_sprung + self.vehicle.prop.m_unsprung) * 9.81 / 4)
-            #         print(downforce)
-            #         print((self.vehicle.prop.m_sprung + self.vehicle.prop.m_unsprung) * 9.81 / 4 + downforce)
-
-            # self.opti.set_initial(
-            #     Z[k][:, :], (self.vehicle.prop.m_sprung + self.vehicle.prop.m_unsprung) * 9.81 / 4
-            # )
-
 
             # q4
             self.opti.set_initial(
@@ -145,24 +145,30 @@ class MLTCollocation(PSCollocation):
             # Fxa equal with initial drag
             self.opti.set_initial(
                 U[k][:, 0],
-                0.5 * self.vehicle.env.rho * self.vehicle.prop.g_S * self.vehicle.prop.a_Cx * v_guess**2,
+                # self.vehicle.prop.e_max / v_guess,
+                0.5
+                * self.vehicle.env.rho
+                * self.vehicle.prop.g_S
+                * self.vehicle.prop.a_Cx
+                * v_guess**2,
             )
 
             # delta
-            # curvature = np.sqrt(np.sum(self.track.der_state(t_tau * self.track.length, 2)[:, :3]**2, axis=1))
-            # normal = self.track.normal(self.track(t_tau))
-            # tangent = self.track.der_state(t_tau)[:, :3]
-            # b = np.cross(normal, tangent, axis=1)
- 
-            # curvature[b[:, 2] > 0] *= -1
-            # wheelbase = sum(self.vehicle.prop.g_a)
-            # delta_guess = np.atan(wheelbase * curvature)
-            # self.opti.set_initial(U[k][:, 2], delta_guess)
+            curvature = np.sqrt(
+                np.sum(self.track.der_state(t_tau * self.track.length, 2)[:, :3] ** 2, axis=1)
+            )
+            b = self.track.tnb_better(t_tau * self.track.length)[2]
 
-            s_points = t_tau * self.track.length
+            curvature[b[:, 2] < 0] *= -1
             wheelbase = sum(self.vehicle.prop.g_a)
-            kyaw = self.track.der_state(s_points, n=1)[:, 3]
-            delta_guess = np.atan(wheelbase * kyaw)
+            delta_guess = np.atan(wheelbase * curvature)
+
+            self.opti.set_initial(U[k][:, 2], delta_guess)
+
+            # s_points = t_tau * self.track.length
+            # wheelbase = sum(self.vehicle.prop.g_a)
+            # kyaw = self.track.der_state(s_points, n=1)[:, 3]
+            # delta_guess = np.atan(wheelbase * kyaw)
 
             # self.opti.set_initial(U[k][:, 2], delta_guess)
 
@@ -176,18 +182,17 @@ class MLTCollocation(PSCollocation):
         self.opti.minimize(J)
 
         ipopt_settings = {
-            "ipopt.print_frequency_iter": 50,
+            # "ipopt.print_frequency_iter": 50,
             "print_time": 0,
             "ipopt.sb": "no",
-            "ipopt.max_iter": 1000,
+            "ipopt.max_iter": 2000,
             "detect_simple_bounds": True,
             "ipopt.linear_solver": "ma97",
             "ipopt.mu_strategy": "adaptive",
             "ipopt.nlp_scaling_method": "gradient-based",
             "ipopt.bound_relax_factor": 1e-3,
-            # "ipopt.hessian_approximation": "exact",
-
-            "ipopt.hessian_approximation": "limited-memory",
+            "ipopt.hessian_approximation": "exact",
+            # "ipopt.hessian_approximation": "limited-memory",
             "ipopt.limited_memory_max_history": 10,
             "ipopt.derivative_test": "none",
         }
@@ -216,6 +221,37 @@ class MLTCollocation(PSCollocation):
             stats = sol.stats()
             print(f"Solve iteration failed after {stats['iter_count']} iteration...")
 
+            g = self.opti.debug.value(self.opti.g)
+            print(f"Initial infeasibility: max |g| = {np.max(np.abs(g)):.3e}")
+            # Diagnostics
+            worst_20 = np.argsort(np.abs(g))[-200:][::-1]
+            constraint_names = [
+                "fz_implicit_0",
+                "fz_implicit_1",
+                "fz_implicit_2",
+                "fz_implicit_3",
+                "ff_torque",
+                "road_lat",
+                "yaw",
+                "vert",
+                "pitch",
+                "roll",
+                "min_speed",
+                "power_limit",
+                "fxa_pos",
+                "fxb_pos",
+                "steer_bounds",
+                "track_bounds",
+                "friction_ellipse_0",
+                "friction_ellipse_1",
+                "friction_ellipse_2",
+                "friction_ellipse_3",
+            ]
+            for idx in worst_20:
+                local = idx % 20
+                point = idx // 20
+                print(f"  g[{idx}] = {g[idx]:+.3e}  ->  {constraint_names[local]}  @ point {point}")
+
         print(f"Final cost: {sol.value(J)}")
 
         # Collect solution
@@ -223,15 +259,18 @@ class MLTCollocation(PSCollocation):
         Q_sol = [sol.value(seg) for seg in Q]
         Z_sol = [sol.value(seg) for seg in Z]
         v_sol = [sol.value(seg) for seg in Q_1_dot]
-        # all_t = np.array(all_t)
 
         # Create Trajectory and save it
         return Trajectory(Q_sol, U_sol, Z_sol, v_sol, t, self.track.length)
 
     @staticmethod
     def cost(q_1_dot, u, prev_u, k_delta=1e-5, k_f=1e-3):
-        return 1 / q_1_dot + k_f * (u[0] * u[1]) + k_delta * ca.sqrt((u[2] - prev_u[2])**2 + 1e-8)
-    
+        return (
+            1 / ca.sqrt((q_1_dot + 1e-5) ** 2)
+            + k_f * ca.sqrt((u[0] * u[1]) ** 2)
+            + k_delta * ca.sqrt((u[2] - prev_u[2]) ** 2 + 1e-8)
+        )
+
     def sample_cost(self, traj: Trajectory, points: np.ndarray) -> tuple[np.ndarray, float]:
         """
         Samples costs at the given points and computes their trapezoidal quadrature
@@ -245,28 +284,22 @@ class MLTCollocation(PSCollocation):
         """
 
         # Measures runge phenomena at a point
-        
         state = traj(points * traj.length)
         linstate = traj.linstate(points * traj.length)
-
-        # print(state, linstate)
 
         v_poly = state[:, -1]
         v_lin = linstate[:, -1]
 
-        fz_poly = state[:, -2]   # just measure one wheel
+        fz_poly = state[:, -2]  # just measure one wheel
         fz_lin = linstate[:, -2]
 
         v_defect = ((v_poly - v_lin) / (np.abs(v_lin) + 1e-5)) ** 2
         fz_defect = ((fz_poly - fz_lin) / (np.abs(fz_lin) + 1e-5)) ** 2
 
-        # print(v_defect, fz_defect)
-
         return v_defect + fz_defect, np.trapezoid((v_defect + fz_defect), x=points)
 
 
 if __name__ == "__main__":
-
 
     config = {
         "track": "track_import/generated/track.json",
@@ -290,15 +323,14 @@ if __name__ == "__main__":
 
     # traj = mr.run()
 
-    foo = MLTCollocation(config)
-    foo.iteration(np.linspace(0, 1, 100), np.array([4] * 99)).save("mlt/generated/testing.json")
-
+    mlt = MLTCollocation(config)
+    mlt.iteration(np.linspace(0, 1, 120), np.array([5] * 119)).save("mlt/generated/testing.json")
 
     props = VehicleProperties.load_yaml("mlt/vehicle_properties/DallaraAV24.yaml")
     traj = Trajectory.load("mlt/generated/testing.json")
 
     # Visualize
-    fine_plot, _ = foo.track.plot_uniform(1)
+    fine_plot, _ = mlt.track.plot_uniform(1)
 
     traj.plot_collocation()
 
@@ -307,12 +339,12 @@ if __name__ == "__main__":
     fig.add_traces(
         [
             *fine_plot,
-            foo.track.plot_raceline_colloc(traj),
+            mlt.track.plot_raceline_colloc(traj),
             # foo.track.plot_raceline_uniform(traj),
-            *foo.track.plot_car_bounds(traj, props.g_t),
+            *mlt.track.plot_car_bounds(traj, props.g_t),
         ]
     )
-    fig.add_trace(foo.track.plot_ribbon())
+    fig.add_trace(mlt.track.plot_ribbon())
 
     fig.update_layout(
         scene=dict(

mlt/trajectory.py
@@ -38,20 +38,17 @@ class Trajectory:
         self.t = t * track_length       # q1
         self.length = track_length
         self.colloc_t = []              # all collocation times
-        foo = []
 
         # List of the interpolated polynomial over each interval
         # [fxa, fxb, delta, q2, q3, q4, q5, q6, fz11, fz12, fz21, fz22, v]
         self.poly = []
-        allt = []
 
         for k in range(len(Q)):
             # Number of collocation points
             N_k = len(Q[k]) - 2
             tau, _ = np.polynomial.legendre.leggauss(N_k)
             tau = np.asarray([-1] + list(tau) + [1])
-            self.colloc_t.extend(self.tau_to_t(tau[:-1], k))       # misses last element but thats probably fine
-            foo.extend(self.tau_to_t(tau, k))
+            self.colloc_t.extend(self.tau_to_t(tau, k))       # misses last element but thats probably fine
 
             self.poly.append(
                 scipy.interpolate.BarycentricInterpolator(
@@ -60,8 +57,7 @@ class Trajectory:
             )
         self.colloc_t = np.array(self.colloc_t)
         flattened_all = np.vstack([np.column_stack([U[k], Q[k], self.Z[k], self.v[k]]) for k in range(len(Q))])
-        print(np.array(foo).flatten().shape, flattened_all.shape)
-        self.linfit = scipy.interpolate.interp1d(np.array(foo).flatten(), flattened_all, axis=0)
+        self.linfit = scipy.interpolate.interp1d(self.colloc_t, flattened_all, axis=0)
 
     def __call__(self, s: np.ndarray) -> np.ndarray:
         """
@@ -99,8 +95,9 @@ class Trajectory:
 
 
     # david why this exist it looked balls on track fitting too
+    # aaron you managed to break plotting
     # why not
-    def plot_collocation(self, approx_spacing: float = 0.1, plot_uniform = False, plot_q = False):
+    def plot_collocation(self, approx_spacing: float = 0.1, plot_uniform = True, plot_q = True):
         self.plot_params(self.colloc_t, approx_spacing, plot_uniform, plot_q)
 
 

track_import/track.py
@@ -53,8 +53,8 @@ class Track:
             np.ndarray: Array whose columns are [b_l, b_r, x, y, z]
         """
         state = self.state(s)
-        b_l, b_r = self._find_boundaries(state)
-        return np.column_stack([b_l, b_r, state[:, :3]])
+        b_l, b_r = self._find_boundaries(s)
+        return np.column_stack([b_l, b_r, state[:, :3]]) 
 
     def state(self, s: np.ndarray) -> np.ndarray:
         """
@@ -68,7 +68,6 @@ class Track:
                         for each given arc length parameter
         """
         s = s % self.length
-        # k = np.searchsorted(self.t[1:], s)
 
         tau, k = self.t_to_tau(s)
         return np.asarray([self.poly[interval](parameter) for parameter, interval in zip(tau, k)])
@@ -157,60 +156,55 @@ class Track:
         rots = R.from_euler("ZYX", e_angles).as_matrix()
 
         return [rots[:, :, i] for i in range(3)]
+    
+    def tnb_better(self, s):
+        n = self.der_state(s, n=2)[:, :3]
 
-    # TODO refactor stuff that uses this to use tnb (it is cleaner, this yucky)
-    def normal(self, state):
-        # State is in the form [[x, y, z, theta, mu, phi, n_l, n_r], ...]
-        theta = state[:, 3]
-        mu = state[:, 4]
-        phi = state[:, 5]
+        t = self.der_state(s)[:, :3]
 
-        return np.column_stack(
-            [
-                np.cos(theta) * np.sin(mu) * np.sin(phi) - np.sin(theta) * np.cos(phi),
-                np.sin(theta) * np.sin(mu) * np.sin(phi) + np.cos(theta) * np.cos(phi),
-                np.cos(mu) * np.sin(phi),
-            ]
-        )
+        b = np.cross(t, n, axis=1)
 
-    def _find_boundaries(self, state: np.ndarray) -> tuple[float, float]:
+        return t, n, b
+
+
+    def _find_boundaries(self, s: np.ndarray) -> tuple[float, float]:
         """
         Computes track boundaries
 
         Args:
-            state (np.ndarray): Array of states at each point
-                                [[x, y, z, theta, mu, phi, n_l, n_r], ...]
+            s (np.ndarray): Arc length parameters to evaluate boundaries at
 
         Returns:
             tuple: Tuple of left and right boundaries (b_l, b_r)
         """
         # State is in the form [[x, y, z, theta, mu, phi, n_l, n_r], ...]
+        state = self.state(s)
         x = state[:, :3]
         n_l = state[:, 6]
         n_r = state[:, 7]
 
-        n = self.normal(state)
+        n = self.tnb(s)[1]
 
         b_l = x + n * n_l[:, np.newaxis]
         b_r = x + n * n_r[:, np.newaxis]
 
         return b_l, b_r
 
-    def raceline(self, state: np.ndarray, lateral_displacement: float) -> np.ndarray:
+    def raceline(self, s: np.ndarray, lateral_displacement: float) -> np.ndarray:
         """
         Computes raceline
 
         Args:
-            state (np.ndarray): Array of states at each point
-                                [[x, y, z, theta, mu, phi, n_l, n_r], ...]
+            s (np.ndarray): Arc length parameters
 
 
         Returns:
         """
         # State is in the form [[x, y, z, theta, mu, phi, n_l, n_r], ...]
+        state = self.state(s)
         x = state[:, :3]
 
-        n = self.normal(state)
+        n = self.tnb(s)[1]
 
         raceline_point = x + n * lateral_displacement[:, np.newaxis]
 
@@ -292,7 +286,7 @@ class Track:
             y=np.array([points[:, 1], points[:, 4]]),
             z=np.array([points[:, 2], points[:, 5]]),
             opacity=0.9,
-            colorscale=[[0, "#D3D3D3"], [1, "#D3D3D3"]],
+            colorscale=[[0, "#797979"], [1, "#D3D3D3"]],
             showscale=False,
         )
 
@@ -437,10 +431,8 @@ class Track:
         ss = trajectory.state(s)
         heading_angle = ss[:, 4]
 
-        r = self.raceline(self.state(s), ss[:, 3])
+        r = self.raceline(s, ss[:, 3])
         t, _, b = self.tnb(s)
-
-        # print(t[0], b[0])
 
         # calculation of heading vec for car
         heading_trans = R.from_rotvec(b * heading_angle[:, np.newaxis])
@@ -450,7 +442,6 @@ class Track:
         n_v = np.cross(b, h_v)
         n_v = n_v / np.linalg.norm(n_v, axis=1, keepdims=True)  # normalize just in case
 
-        print(n_v[0])
         width = max(g_t) / 2
 
         r += np.array([0, 0, 0.1])
@@ -504,16 +495,11 @@ class Track:
         Returns:
             go.Scatter3d: Raceline trajectory graph
         """
-        print("len", len(trajectory.colloc_t))
+
         r = self.raceline(
-            self.state(self.length * trajectory.colloc_t),
+            self.length * trajectory.colloc_t,
             trajectory.state(trajectory.colloc_t * self.length)[:, 3],
         )
-        print("len2", len(r))
-        print(r)
-        print(len(r[:, 0]))
-        print(len(r[:, 1]))
-        print(len(r[:, 2]))
 
         return go.Scatter3d(
             x=r[:, 0].flatten(),
@@ -521,5 +507,5 @@ class Track:
             z=r[:, 2].flatten(),
             name="colloc",
             mode="markers",
-            marker=dict(size=5),
+            # marker=dict(size=5),
         )